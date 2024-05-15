826664beb736401bd98e71247b9a0a717ebc12bd
Daniel Clavijo Núñez <daniclavijonunez@gmail.com>
2024-05-15 13:58:45 +0200

    Update sweetAlertLogin.js

--- JS/sweetAlertLogin.js
+++ JS/sweetAlertLogin.js
@@ -6,12 +6,14 @@ document
     const contrasena = document.querySelector('input[name="contrasena"]').value;
     if (usuario.trim() === "" || contrasena.trim() === "") {
       Swal.fire({
+        allowOutsideClick: false,
         title: "Error",
         text: "Por favor, complete todos los campos.",
         icon: "error",
       });
     } else {
       Swal.fire({
+        allowOutsideClick: false,
         title: "Muchas gracias por entrar a Cedago",
         text: "¡Esperamos que disfrutes la experiencia!",
         icon: "success",
